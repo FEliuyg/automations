6785e0cb46aa28fc368145102a7aa9c1cb491ed4
xiaoyao <15558110117@163.com>
2023-04-05 11:44:05 +0800

    fix: update workflow

--- src/utils/dingtalk.js
+++ src/utils/dingtalk.js
@@ -1,11 +1,14 @@
 import crypto from 'crypto';
 import axios from 'axios';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const secretKey = process.env.DINGTALK_SECRET;
-// const secretKey = 'SEC7f84246f184cbbfc31b440af387f5a3fae5ea740b4ac012ac3e80ff4b15de718';
-
 const baseUrl = process.env.DINGTALK_WEBHOOK_URL;
-// const baseUrl = 'https://oapi.dingtalk.com/robot/send?access_token=590840665ac558a2c61644ac6c595f1d54e4e484017619f6a8e28bcf0f20605c';
+
+console.log('secretKey', secretKey);
+console.log('baseUrl', baseUrl);
 
 export async function sendDingtalkMessage(message) {
   const time = Date.now();
